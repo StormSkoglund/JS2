1f7a6f26c0a7d15041839fa67792b95a2bedb7d3
Alex Storm Skoglund <alesko52196@stud.noroff.no>
2024-02-12 17:48:20 +0100

    adds citation to sources used for the  preventrefresh function

--- JavaScript/register.js
+++ JavaScript/register.js
@@ -55,6 +55,7 @@ function getInputs() {
   registerUser(`${API_BASE_URL}/api/v1/social/auth/register`, user);
 }
 // addEventListener Two Functions (2024) . [Online] available at Stack Overflow https://stackoverflow.com/questions/25028853/addeventlistener-two-functions (Accessed: 12 February 2024).
+// how-to-stop-refreshing-the-page-on-submit-in-javascript (2024) [Online] available at Tutorials Point https://www.tutorialspoint.com/how-to-stop-refreshing-the-page-on-submit-in-javascript (Accessed: 12 February 2024).
 function preventFormRefresh(event) {
   event.preventDefault();
 }
