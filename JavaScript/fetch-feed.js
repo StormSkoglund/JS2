bd57d7b27e67feaf3051b69e83c8f841240487b0
Alex Storm Skoglund <alesko52196@stud.noroff.no>
2024-02-20 16:59:05 +0100

    Adds new shadow variable to index.scss and changes to styling on feed.

--- JavaScript/fetch-feed.js
+++ JavaScript/fetch-feed.js
@@ -21,12 +21,12 @@ async function fetchAuthorized(url) {
     console.log(posts);
 
     posts.forEach((post) => {
-      displayFeed.innerHTML += `<article class="card m-2 p-3 "><span class="" ><h6 class="text-center fs-4">${post.title}</h6></span><span><hr/><div><p class="fs-5" >${post.body}</p><p class="fs-6 fw-lighter">Tags: ${post.tags}, </p></span> </article>`;
-      const postImage = document.createElement("img");
-      postImage.className = "col-12 border border-info border-3";
-      postImage.src = post.media;
-      postImage.alt = post.title;
-      displayFeed.append(postImage);
+      displayFeed.innerHTML += `<article class="card m-2 p-3 mt-4 mb-4 box-shadow-light"><span><h6 class="text-center fs-1">${post.title}</h6></span><span><hr class="border border-primary border-2 opacity-75"/><div><p class="fs-5" >${post.body}</p><img class="col-12 rounded-3 shadow-lg" src="${post.media}"><p class="fs-6 fw-lighter"> ${post.tags} </p></span> </article>`;
+      //const postImage = document.createElement("img");
+      //postImage.className = "col-12 border border-info border-3";
+      //postImage.src = post.media;
+      //postImage.alt = post.title;
+      //displayFeed.append(postImage);
     });
   } catch (error) {
     console.log(error);
