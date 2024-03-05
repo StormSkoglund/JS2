8f2605fd4640eeba168d997dc40aaba06e02d805
Alex Storm Skoglund <alesko52196@stud.noroff.no>
2024-03-05 14:57:53 +0100

    Adds a search feature to fetch-feed.js

--- JavaScript/fetch-feed.js
+++ JavaScript/fetch-feed.js
@@ -4,8 +4,11 @@ import { fetchAuthorized } from "./modules/fetch-content.mjs";
 import { API_BASE_URL } from "./modules/inputs.mjs";
 import { delayRefreshPage } from "./modules/norefresh.mjs";
 import { postContent } from "./modules/post.mjs";
+import { tryCatchError } from "./modules/error.mjs";
 
 displayContent(API_BASE_URL + "/api/v1/social/posts?_author=true");
+
+let posts = [];
 
 consts.postBtn.addEventListener("click", postContent);
 
@@ -25,17 +28,115 @@ consts.dropDate.addEventListener("change", async function () {
   }
 });
 
-//Search funtionality
-//async function searchByInput() {
-//  let posts = await fetchAuthorized();
-//  const searchValue = consts.searchBar.value;
-//  console.log(searchValue);
-//}
+// Search function Demonstrated by Martin Krüger on February 27, 2024. Adapted to suit my API call and content rendering.
 
-//consts.searchBar.addEventListener("value", searchByInput);
+async function displayContentFiltered(posts, userInput = "") {
+  console.log(userInput);
+  try {
+    posts = await fetchAuthorized(
+      API_BASE_URL + "/api/v1/social/posts?_author=true"
+    );
+    consts.displayFeed.innerHTML = "";
+    let filteredPosts = posts.filter((post) => {
+      if (userInput === "") {
+        return true;
+      }
+      if (post.title.toLowerCase().startsWith(userInput.toLowerCase())) {
+        return true;
+      }
+      return false;
+    });
+    filteredPosts.forEach((post) => {
+      /*I am formatting the date from the API . Available at https://stackoverflow.com/questions/58791663/how-to-modify-date-format-taken-from-wordpress-api[Viewed Nov 17. 2023]. And at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString[Viewed Nov 17. 2023] */
+      let formatDate = new Date(post.updated).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      });
+
+      // using Martin Krügers example with a default image, where there is no media to be fetched.
+      let fetchedMedia = post.media || consts.DEFAULT_IMAGE_URL;
+      let fetchedAvatar = post.author.avatar || consts.DEFAULT_IMAGE_AVATAR_URL;
+
+      let article = document.createElement("article");
+      article.className = "card m-2 p-3 mt-4 mb-4 box-shadow-light clearfix";
+
+      let span1 = document.createElement("span");
+      let span2 = document.createElement("span");
+      let singlePostLink = "../feed/single-post/index.html?";
+
+      let postLink = document.createElement("a");
+      postLink.href = singlePostLink + "id=" + post.id;
+      postLink.className = "stretched-link";
+      span1.appendChild(postLink);
+
+      let userImage = document.createElement("img");
+      userImage.src = fetchedAvatar;
+      userImage.className =
+        "rounded-circle img-fluid col-4 col-md-3 border-info img-thumbnail d-block m-auto border-4 border-info opacity-75";
+      span1.appendChild(userImage);
+
+      let userName = document.createElement("p");
+      userName.textContent = post.author.name;
+      userName.className = "fs-6 text-center";
+      span1.appendChild(userName);
+
+      let h6 = document.createElement("h6");
+      h6.className = "text-center fs-1";
+      h6.textContent = post.title;
+      span1.appendChild(h6);
+
+      article.appendChild(span1);
+
+      let hr1 = document.createElement("hr");
+      hr1.className = "border border-primary border-2 opacity-75";
+      span2.appendChild(hr1);
+
+      let div = document.createElement("div");
+      let p = document.createElement("p");
+      p.className = "fs-5";
+      p.textContent = post.body;
+      div.appendChild(p);
+      span2.appendChild(div);
+
+      let img2 = document.createElement("img");
+      img2.className = "col-12 rounded-3 shadow-lg";
+      img2.src = fetchedMedia;
+      span2.appendChild(img2);
+
+      let span3 = document.createElement("span");
+      span3.className = "fs-6 fw-lighter";
+      span3.textContent = post.tags;
+      span2.appendChild(span3);
+
+      let hr2 = document.createElement("hr");
+      hr2.className = "border border-primary border-2 opacity-75";
+      span2.appendChild(hr2);
+
+      let span4 = document.createElement("span");
+      span4.className = "fs-6 fw-bold d-inline float-right";
+      span4.textContent = formatDate;
+      span2.appendChild(span4);
+
+      article.appendChild(span2);
+
+      consts.displayFeed.appendChild(article);
+    });
+  } catch (error) {
+    tryCatchError(error);
+  }
+}
+
+function processSearchInput(event) {
+  const userInput = event.currentTarget.value;
+  displayContentFiltered(posts, userInput);
+}
+
+consts.searchByUserInput.addEventListener("keyup", processSearchInput);
 
 //Update post
 
 //delete post
-
-//View post by ID
--- JavaScript/modules/consts.mjs
+++ JavaScript/modules/consts.mjs
@@ -9,5 +9,5 @@ export const displayFeed = document.getElementById("feedContainer");
 export const DEFAULT_IMAGE_URL = "https://picsum.photos/1000";
 export const DEFAULT_IMAGE_AVATAR_URL =
   "https://img.freepik.com/free-psd/3d-icon-social-media-app_23-2150049569.jpg?t=st=1709563197~exp=1709566797~hmac=b689e5e177c3d6c62da590cd067d75d9ba4e97609d314b94de53d37a913bf5a5&w=740";
-export const searchBar = document.getElementById("search");
+export const searchByUserInput = document.getElementById("search");
 export const dropDate = document.getElementById("dropDown");
