ffeda6b677f46829cf0e2eb87773e5963362cf7a
Alex Storm Skoglund <alesko52196@stud.noroff.no>
2024-03-05 20:23:59 +0100

    Adds new function deletePosts

--- JavaScript/modules/deletePosts.mjs
+++ JavaScript/modules/deletePosts.mjs
@@ -3,14 +3,15 @@ import { preventFormRefresh } from "./norefresh.mjs";
 async function deletePost(id, url, event) {
   preventFormRefresh(event);
   try {
+    id = id;
     const getData = {
-      method: "GET",
+      method: "delete",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${consts.token}`,
       },
     };
-    const response = await fetch(url, getData);
+    const response = await fetch(url, id, getData);
     console.log(response);
   } catch (error) {
     tryCatchError(error);
